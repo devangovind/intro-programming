fdfda46a77154a636766b21e3c9644cea54b001f
Jiaxin He <hejiaxin.jh@gmail.com>
2023-11-16 20:47:53 +0000

    'final draft of feature e'

--- classes/Admin.py
+++ classes/Admin.py
@@ -9,56 +9,74 @@ class Admin:
     pass
 
   def __init__(self):
-      self.camps_file = '/Users/jiaxinhe/Library/Mobile Documents/com~apple~CloudDocs/UCL CS/Term 1/COMP0066 Introductory to programming/Groupwork/github_location/intro-programming/files/camps.csv'
-      self.resources_file = '/Users/jiaxinhe/Library/Mobile Documents/com~apple~CloudDocs/UCL CS/Term 1/COMP0066 Introductory to programming/Groupwork/github_location/intro-programming/files/resources.csv'
+        self.camps_file = 'files/camps.csv'
+        self.resources_file = 'files/resources.csv'
+        self.camp_id = None
 
   def read_csv(self, filepath):
-      with open(filepath, mode='r') as file:
-          return list(csv.DictReader(file))
+    try:
+        with open(filepath, mode='r', encoding='utf-8-sig') as file:  # 'utf-8-sig' handles the BOM
+            return list(csv.DictReader(file))
+    except Exception as e:
+        print(f"Error reading file {filepath}: {e}")
+        return []
 
   def write_csv(self, filepath, data):
-      with open(filepath, mode='w', newline='') as file:
-          writer = csv.DictWriter(file, fieldnames=data[0].keys())
-          writer.writeheader()
-          writer.writerows(data)
+      try:
+          with open(filepath, mode='w', newline='') as file:
+              writer = csv.DictWriter(file, fieldnames=data[0].keys())
+              writer.writeheader()
+              writer.writerows(data)
+      except Exception as e:
+          print(f"Error writing file {filepath}: {e}")
 
-  def get_camp_population(self, camp_id):
-    camps = self.read_csv(self.camps_file)
-    for camp in camps:
-        if 'camp_id' in camp and camp['camp_id'] == camp_id:
-            return int(camp['population'])
-    return 0
+  def set_camp_id(self):
+      self.camp_id = input("Enter camp ID: ")
 
-  def update_resource_allocation(self, camp_id, resource_type, quantity):
-    if not isinstance(camp_id, str) or not isinstance(resource_type, str):
-        raise ValueError("camp_id and resource_type must be strings")
+  def get_camp_population(self):
+    try:
+        camps = self.read_csv(self.camps_file)
+        for camp in camps:
+            if 'camp_id' in camp and camp['camp_id'] == self.camp_id:
+                return int(camp['population'])
+        print(f"Camp ID: {self.camp_id} not found.")
+        return 0
+    except Exception as e:
+        print(f"Error in get_camp_population: {e}")
+        return 0
 
-    resources = self.read_csv(self.resources_file)
-    updated = False
-    for column in resources:
-        if column.get('camp_id') == camp_id:
-            if resource_type in column:
-                column[resource_type] = quantity
-                updated = True
-            else:
-                raise ValueError(f"Invalid resource type: {resource_type}")
+  def update_resource_allocation(self, resource_type, quantity):
+    try:
+        if not isinstance(self.camp_id, str) or not isinstance(resource_type, str):
+            raise ValueError("camp_id and resource_type must be strings")
 
-    if updated:
-        print(f"Updated resources for {camp_id}: {resources}")
-        self.write_csv(self.resources_file, resources)
-    else:
-        print(f"No changes made for {camp_id}. Camp ID may not exist in the file.")
+        resources = self.read_csv(self.resources_file)
+        camp_exists = False
+        for column in resources:
+            if column.get('camp_id') == self.camp_id:
+                camp_exists = True
+                if resource_type in column:
+                    column[resource_type] = quantity
+                    print(f"Updated {resource_type} for {self.camp_id} to {quantity}.")
+                    self.write_csv(self.resources_file, resources)
+                    return
+                else:
+                    raise ValueError(f"Invalid resource type: {resource_type}")
+
+        if not camp_exists:
+            print(f"Camp ID {self.camp_id} does not exist in the file.")
+
+    except Exception as e:
+        print(f"Error in update_resource_allocation: {e}")
 
 
-
-  def allocate_resources(self, camp_id, duration_days=7):
-    population = self.get_camp_population(camp_id)
-    
+  def suggest_resources(self, duration_days=7):
+    population = self.get_camp_population()
     if population > 0:
         # Define basic needs
-        food_per_person_per_day = 3  # 3 food packets per person per day
-        medical_kits_per_10_people_per_day = 0.1  # 1 kit per 10 people per day
-        people_per_tent = 5  # Assuming 5 people per tent
+        food_per_person_per_day = 3  # e.g., 3 food packets per person per day
+        medical_kits_per_10_people_per_day = 0.1  # e.g., 1 kit per 10 people per day
+        people_per_tent = 5  # e.g., Assuming 5 people per tent
 
         # Calculate total needs
         total_food_needed = food_per_person_per_day * population * duration_days
@@ -71,17 +89,23 @@ class Admin:
         total_medical_kits_needed = int(total_medical_kits_needed * buffer)
         total_tents_needed = int(total_tents_needed * buffer)
 
-        # Update resource allocation
-        self.update_resource_allocation(camp_id, 'food_packets', total_food_needed)
-        self.update_resource_allocation(camp_id, 'medical_kits', total_medical_kits_needed)
-        self.update_resource_allocation(camp_id, 'tents', total_tents_needed)
+        return {"food_pac": total_food_needed, "medical_sup": total_medical_kits_needed, "tents": total_tents_needed}
+    else:
+        print(f"No suggestions for camp_id {self.camp_id} with population 0.")
+        return {}
 
-        return {"food_packets": total_food_needed, "medical_kits": total_medical_kits_needed, "tents": total_tents_needed}
+
+  def manual_resource_allocation(self):
+      if not self.camp_id:
+          self.set_camp_id()
+      suggested_resources = self.suggest_resources()
+      for resource_type, suggested_quantity in suggested_resources.items():
+          print(f"Suggested quantity for {resource_type}: {suggested_quantity}")
+          user_input = input(f"Enter quantity for {resource_type} (press Enter to accept suggestion): ")
+          quantity = int(user_input) if user_input else suggested_quantity
+          self.update_resource_allocation(resource_type, quantity)
 
 if __name__ == "__main__":
     admin = Admin()
-    camp_id = 'camp_01'
-    print(f"Allocating resources for {camp_id}:")
-    admin.allocate_resources(camp_id)
-    admin.update_resource_allocation(camp_id, 'tents',10)
-    print(f"Resources allocated successfully for {camp_id}.")
+    admin.set_camp_id()  # Set camp_id once
+    admin.manual_resource_allocation() 
