ce4aeb6bdce4890b9290fbdb213ad51f4583746c
Devan Govind <devangovind@gmail.com>
2023-11-18 23:42:00 +0000

    added overwriting (edit function)

--- classes/Volunteer.py
+++ classes/Volunteer.py
@@ -5,26 +5,36 @@ from Camps import Camps
 class Volunteer:
     def __init__(self, username):
         self.username = username
-        
     def get_volunteer_data(self):
-        volunteer_file = pd.read_csv('./files/volunteers.csv')
-        self.volunteer_data = volunteer_file[volunteer_file['Username'] == self.username]
+        self.volunteer_file = pd.read_csv('./files/volunteers.csv')
+        self.volunteer_data = self.volunteer_file[self.volunteer_file['Username'] == self.username].copy()
+        self.volunteer_index = self.volunteer_data.index
         if not self.volunteer_data.empty: return (self.volunteer_data)
         else: return "No volunteer data"
-    def edit_volunteer_data(self, name, phone, age, campID, availability):
+    def edit_volunteer_data(self, fname, sname, phone, age, campID, availability):
         try: 
-            self.validate_data(name, phone, age, campID, availability)
+            self.validate_data(fname, sname, phone, age, campID, availability)
             print("data all good")
+            self.volunteer_data['First Name'] = fname
+            self.volunteer_data['Last Name'] = sname
+            self.volunteer_data['Phone'] = phone
+            self.volunteer_data['Age'] = age
+            self.volunteer_data['CampID'] = campID
+            self.volunteer_data['Availabiltiy'] = availability
+            self.volunteer_file.iloc[self.volunteer_index, :] = self.volunteer_data
+            self.volunteer_file.to_csv('./files/volunteers.csv', index=False)
         except ValueError as e:
             print(f'errors: {e}')
     
 
         
-    def validate_data(self, name, phone, age, campID, availability):
+    def validate_data(self, fname, sname, phone, age, campID, availability):
         alphabet = "^[a-zA-Z\s]+$"
-        def name_validate():
+        def name_validate(name):
             if not bool(re.match(alphabet, name)):
                 raise ValueError("Name can only be characters")
+            if " " in name:
+                raise ValueError("Do not enter spaces in name")
         def phone_validate():
             try:
                 int(phone)
@@ -45,7 +55,7 @@ class Volunteer:
             camps = camp_data['camp_id']
             if campID not in camps.values:
                 raise ValueError("CampID does not exist")
-        return name_validate(), phone_validate(), age_validate(), camp_validate()
+        return name_validate(fname), name_validate(sname), phone_validate(), age_validate(), camp_validate()
     def create_refugee_profile(self):
         pass
     def edit_details(self):
@@ -53,7 +63,7 @@ class Volunteer:
 
 volunteer = Volunteer('volunteer1')
 volunteer.get_volunteer_data()
-volunteer.edit_volunteer_data("dev", 1, 3, "cap", "01")
-volunteer.edit_volunteer_data("dev2", "A", "3", "cap", "01")
-volunteer.edit_volunteer_data("  ", "sad", "324s2", "cap", "01")
+volunteer.edit_volunteer_data("dev", "gov", 1, 3, "camp_01", "01")
+# volunteer.edit_volunteer_data("dev2", "A", "3", "cap", "01")
+# volunteer.edit_volunteer_data("  ", "sad", "324s2", "cap", "01")
 
