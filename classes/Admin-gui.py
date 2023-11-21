0bd7fc9f43d4bb914359f0130fca41b1ffb769b5
Devan Govind <devangovind@gmail.com>
2023-11-21 18:03:57 +0000

    created base template

---
+++ classes/Admin-gui.py
@@ -0,0 +1,69 @@
+from Admin import Admin
+import tkinter as tk
+from tkinter import messagebox
+
+class AdminGui:
+    def __init__(self, admin):
+        self.root = tk.Tk()
+        self.root.geometry("800x600")
+        self.root.title("Volunteer View")
+        self.admin = admin
+        # self.volunteer_data = self.volunteer.get_volunteer_data()
+        self.create_nav_bar()
+        self.welcome_message()
+        # self.edit_details_button = tk.Button(self.root, text="Edit personal details", font=('Arial', 20))
+    def create_nav_bar(self):
+        self.headerarea = tk.Frame(self.root)
+        self.headerarea.columnconfigure(0, weight=1)
+        self.headerarea.columnconfigure(1, weight=1)
+        self.headerarea.columnconfigure(2, weight=1)
+        self.headerarea.columnconfigure(3, weight=1)
+        self.headerarea.columnconfigure(4, weight=1)
+        self.headerarea.columnconfigure(5, weight=1)
+        self.home_btn = tk.Button(self.headerarea, text="Home", font=('Arial', 16), command=self.welcome_message)
+        self.home_btn.grid(row=0, column=0)
+        self.create_plan_btn = tk.Button(self.headerarea, text="Create New Plan", font=('Arial', 16), command=self.create_new_plan)
+        self.create_plan_btn.grid(row=0, column=1)
+        self.display_plans_btn = tk.Button(self.headerarea, text="Display Existing Plans", font=('Arial', 16), command=self.display_plans)
+        self.display_plans_btn.grid(row=0, column=2)
+        self.manage_camps_btn = tk.Button(self.headerarea, text="Manage Camps", font=('Arial', 16))
+        self.manage_camps_btn.grid(row=0, column=3)
+        self.manage_volunteers_btn = tk.Button(self.headerarea, text="Manage Volunteers", font=('Arial', 16))
+        self.manage_volunteers_btn.grid(row=0, column=4)
+        self.logout_btn = tk.Button(self.headerarea, text="Logout", font=('Arial', 16))
+        self.logout_btn.grid(row=0, column=5)
+        self.headerarea.pack(padx=20)
+        self.nav_bar = [self.headerarea, self.home_btn,self.display_plans_btn, self.create_plan_btn, self.manage_camps_btn, self.manage_volunteers_btn, self.logout_btn]
+    def welcome_message(self):
+        self.clear_content()
+        welcome_back = f'Welcome Back, Admin'
+        label = tk.Label(self.root, text=welcome_back, font=('Arial', 24))
+        label.pack(pady=100)
+    def create_new_plan(self):
+        self.clear_content()
+        # add the code for creating a new plan here
+    def display_plans(self):
+        self.clear_content()
+        # add code here:
+        # somewhere in here will be the end button for the individual plans which will maybe go to another function 
+
+    def manage_volunteers(self):
+        self.clear_content()
+        # add code here to edit volunteer data
+
+    def manage_camps(self):
+        self.clear_content()
+        # code here to manage camp (alllocate resources etc.)
+
+    def clear_content(self):
+        for widget in self.root.winfo_children():
+            if widget not in self.nav_bar:
+                widget.destroy()
+    def run(self):
+    
+        self.root.mainloop()
+
+dummy = Admin()
+VGui = AdminGui(dummy)
+VGui.run()
+
