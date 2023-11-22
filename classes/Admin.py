e7095a889c9c740e8e56a06816e34311eb285432
j-mendi <zchajdm@ucl.ac.uk>
2023-11-22 22:24:46 +0000

    checked to see if works - added all function

--- classes/Admin.py
+++ classes/Admin.py
@@ -9,33 +9,77 @@ class Admin:
         pass
 
     def __init__(self):
-        self.login_file = '../file/logindetails.csv'
+        self.login_file = './files/logindetails.csv'
         self.users = pd.read_csv(self.login_file)
 
+
     def save_changes(self):
-        self.users.to_csv(self.login_file, index=0)
+        self.users.to_csv(self.login_file, sep=',', index=False, encoding='utf-8')
+        return "Saved changes"
+
 
     def activate_account(self, username):
-        if username in self.users['Username'].values:
-            self.users.loc[self.users['Username'] == username, 'Active'] = True
-            self.save_changes()
-            return 'Account has been activated'
-        else:
+        try:
+            if username in self.users['Username'].values:
+                if self.users.loc[self.users['Username'] == username, 'Active'].iloc[0] != True: # iloc[0] ensures that 'Active' column is selected 
+                    self.users.loc[self.users['Username'] == username, 'Active'] = True
+                    self.save_changes()
+                    return 'Your account has been activated'
+                else:
+                    return 'Your account has already been activated'
+        except KeyError:
             return "Account doesn't exist"
 
+
+    def activate_all(self):
+        self.users['Active'] = True
+        return "All accounts have been activated"
+
+
     def deactivate_account(self, username):
-        if username in self.users['Username'].values:
-            if self.users.loc[self.users['Username'] == username, 'Active'] == True:
-                self.users.replace[self.users['Username'] == username, 'Active'] = False # replace the data onto the data frame
-                self.save_changes()
-            return "Your account has been deactivated, contact the administator."
-        else:
+        try:
+            if username in self.users['Username'].values:
+                if self.users.loc[self.users['Username'] == username, 'Active'].iloc[0] == True:
+                    self.users.loc[self.users['Username'] == username, 'Active'] = False
+                    self.save_changes()
+                    return "Your account has been deactivated, contact the administrator."
+                else:
+                    return "Your account has already been deactivated"
+        except KeyError:
             return "Account doesn't exist"
-        
+
+    def deactivate_all(self):
+        self.users['Active'] = False
+        return "All accounts have been deactivated"
+
+
     def delete_account(self, username):
-        if username in self.users['Username'].values:
-            self.users = self.users[self.users['Username'] == None]
-            self.save_changes()
-            return "Account deleted"
-        else:
+        try:
+            if username in self.users['Username'].values:
+                self.users = self.users[self.users['Username'] != username]  # Filter out the user to be deleted
+                self.save_changes()
+                return f"Account {username} deleted"
+            else:
+                return "Account doesn't exist"
+        except KeyError:
             return "Account doesn't exist"
+
+if __name__ == "__main__":
+    admin = Admin()
+    print(admin.users)
+
+    print(admin.deactivate_all())
+    print(admin.users)
+
+    
+    print(admin.activate_all())
+    print(admin.users)
+    
+    print(admin.deactivate_account('volunteer1'))
+    print(admin.users)
+    
+    print(admin.activate_account('volunteer1'))
+    print(admin.users)
+
+    #print(admin.delete_account('volunteer3'))
+    #print(admin.users)
