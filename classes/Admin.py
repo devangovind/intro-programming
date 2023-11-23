6b0ac9b3f3f167a0eaca392e610074afed12f9b2
mpeniket <131975856+mpeniket@users.noreply.github.com>
2023-11-23 18:21:30 +0000

    minor admin rearrangements - still work in progress

--- classes/Admin.py
+++ classes/Admin.py
@@ -8,13 +8,7 @@ from datetime import date
 from datetime import datetime
 
 class Admin:
-
-    # add functions here
-    # remember for functions added the first parameter has to be a self
-    def create_humanitarian_plan(self):
-        pass
-  
-
+    
     def __init__(self):
         self.plans_file = 'intro-programming/files/plans.csv'
         self.camps_file = 'intro-programming/files/camps_file.csv'
@@ -23,10 +17,68 @@ class Admin:
         self.camp_id = None
         self.users = pd.read_csv(self.login_file)
 
+# FOR (A) CREATE PLANS
+
+    def create_humanitarian_plan(self):
+        description = input("Enter the description of the plan: ")
+        geographical_location = input("Enter the geographical location of the plan: ")
+        start_date = input("Enter the start date of the plan (YYYY/MM/DD): ")
+        
+        plan = humanitarian_plan(description, geographical_location, start_date)
+        plan_data = plan.display_plan()
+    
+        plan_dict = {'description': description, 'geographical_location': geographical_location, 'start_date': start_date}
+
+        self.write_csv(self.plans_file, plan_data)
+        
+    def menu(self):
+        while True:
+            print("1. Create a new humanitarian plan")
+            print("2. Display the humanitarian plans")
+
+            a = int(input("Please enter your requirement: "))
+
+            if a == 1:
+                self.create_humanitarian_plan()
+            elif a == 2:
+                self.display_plans()
+            else:
+                print("Please enter the valid number again!")
+                
+    
+    def read_csv(self, filepath):
+        try:
+            with open(filepath, mode='r', encoding='utf-8-sig') as file:  
+                return list(csv.DictReader(file))
+        except Exception as e:
+            print(f"Error reading file {filepath}: {e}")
+            return []
+          
+    
+    def write_csv(self, filepath, data):
+        try:
+            with open(filepath, mode='a', newline='') as file:
+                writer = csv.DictWriter(file, fieldnames=data.keys())
+
+                if file.tell() == 0:
+                    writer.writeheader()
+                
+                writer.writerow(data)
+        except Exception as e:
+            print(f"Error writing file {filepath}: {e}")
+
+
+    def display_plans(self):
+        existing_data = self.read_csv(self.plans_file)
+
+        for row in existing_data:
+            print(row)
+            
+ # FOR (B) DISPLAY PLAN
+ 
 
  # FOR (C) DISPLAY PLAN
-
-
+ 
     def check_event_ended(self, plan_id):
         plans = pd.read_csv(self.plans_file)
         plan_details = plans[plans["Plan_ID"]== plan_id]
@@ -52,8 +104,7 @@ class Admin:
           
  
  # FOR (D) ACCOUNT ACTIVATION
-
-
+ 
     def save_changes(self):
         self.users.to_csv(self.login_file, sep=',', index=False, encoding='utf-8')
         return "Saved changes"
@@ -107,64 +158,7 @@ class Admin:
 
  
  # FOR (E) RESOURCE ALLOCATION
-
-    def read_csv(self, filepath):
-        try:
-            with open(filepath, mode='r', encoding='utf-8-sig') as file:  
-                return list(csv.DictReader(file))
-        except Exception as e:
-            print(f"Error reading file {filepath}: {e}")
-            return []
-          
-    
-    def write_csv(self, filepath, data):
-        try:
-            with open(filepath, mode='a', newline='') as file:
-                writer = csv.DictWriter(file, fieldnames=data.keys())
-
-                if file.tell() == 0:
-                    writer.writeheader()
-                
-                writer.writerow(data)
-        except Exception as e:
-            print(f"Error writing file {filepath}: {e}")
-
-
-    def create_humanitarian_plan(self):
-        description = input("Enter the description of the plan: ")
-        geographical_location = input("Enter the geographical location of the plan: ")
-        start_date = input("Enter the start date of the plan (YYYY/MM/DD): ")
-        
-        plan = humanitarian_plan(description, geographical_location, start_date)
-        plan_data = plan.display_plan()
-    
-        plan_dict = {'description': description, 'geographical_location': geographical_location, 'start_date': start_date}
-
-        self.write_csv(self.plans_file, plan_data)
-
-    
-    def display_plans(self):
-        existing_data = self.read_csv(self.plans_file)
-
-        for row in existing_data:
-            print(row)
-
-
-    def menu(self):
-        while True:
-            print("1. Create a new humanitarian plan")
-            print("2. Display the humanitarian plans")
-
-            a = int(input("Please enter your requirement: "))
-
-            if a == 1:
-                self.create_humanitarian_plan()
-            elif a == 2:
-                self.display_plans()
-            else:
-                print("Please enter the valid number again!")
-
-                
+             
     def set_camp_id(self):
         self.camp_id = input("Enter camp ID (Not case sensitive)): ").upper()
 
