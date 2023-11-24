4b53cf1399474ae2e28ae3bee93f302fc3df3f29
Yan Zhang <zczqyyz@ucl.ac.uk>
2023-11-24 11:47:16 +0000

    admin.py_a-c_update

--- classes/Admin.py
+++ classes/Admin.py
@@ -1,187 +1,157 @@
 # Admin Class File
+import pandas as pd
 import csv
 import datetime
 
-
 class Admin:
-  # add functions here
-  # remember for functions added the first parameter has to be a self
-
-  def create_humanitarian_plan(self):
-    pass
-
-
-
-
-  def __init__(self):
-        self.camps_file = 'files/camps.csv'
-        self.resources_file = 'files/resources.csv'
-        self.camp_id = None
-        self.sdate_save = None
-        self.date_save = None
-        self.month = None
-        self.year = None
+    def __init__(self):
+            with open('C:\\Users\\96249\Desktop\\Python_CW\\intro-programming\\files\\plan_file.csv', 'r', encoding='utf-8') as plan_file:
+                read = csv.DictReader(plan_file)
+                self.plan_list = []
+                for row in read:
+                    self.plan_list.append(row)
+            # print(plan_list)
+            self.camps_file = 'intro-programming/files/camps_file.csv'
+            self.resources_file = 'intro-programming/files/resources.csv'
+            self.camp_id = None
 
 
 
-  def read_csv(self, filepath):
-    try:
-        with open(filepath, mode='r', encoding='utf-8-sig') as file:  # 'utf-8-sig' handles the BOM
-            return list(csv.DictReader(file))
-    except Exception as e:
-        print(f"Error reading file {filepath}: {e}")
-        return []
 
-  def write_csv(self, filepath, data):
-      try:
-          with open(filepath, mode='w', newline='') as file:
-              writer = csv.DictWriter(file, fieldnames=data[0].keys())
-              writer.writeheader()
-              writer.writerows(data)
-      except Exception as e:
-          print(f"Error writing file {filepath}: {e}")
+## Change some functions to fit the admin.gui(for admin feature a-c)
+## This is to find the last plan_id, in ortder to achive planid plus one when admin create a new plan 
+    def last_plan_id(self):
+        plan = pd.read_csv("C:\\Users\\96249\\Desktop\\Python_CW\\intro-programming\\files\plan_file.csv")
 
-  def set_camp_id(self):
-      self.camp_id = input("Enter camp ID: ")
+        res = plan.sort_values(by='Plan_ID', ascending=False)
+        last_plan_id = res.iloc[0]["Plan_ID"]
+        # print(res.iloc[0]["Plan_ID"])
+        return last_plan_id
 
-  def get_camp_population(self):
-    try:
-        camps = self.read_csv(self.camps_file)
-        for camp in camps:
-            if 'camp_id' in camp and camp['camp_id'] == self.camp_id:
-                return int(camp['population'])
-        print(f"Camp ID: {self.camp_id} not found.")
-        return 0
-    except Exception as e:
-        print(f"Error in get_camp_population: {e}")
-        return 0
+## This is to justify the type of the date input
+    def is_date(self, date):
+        return isinstance(date, datetime.date)
+## This is to make sure the start date
+    def check_start_day(self, date):
 
-  def update_resource_allocation(self, resource_type, quantity):
-    try:
-        if not isinstance(self.camp_id, str) or not isinstance(resource_type, str):
-            raise ValueError("camp_id and resource_type must be strings")
+        today = date.today()
+        plan_start_date = date
+        if today <= plan_start_date:
+            return False
+        else:
+            return True
+## This is to make sure the end date
+    def check_end_date(self,end_date,start_day):
+        if end_date > start_day:
+            return False
+        else:
+            return True
+    # def plan_id_plus_1(self):
+    #     self.plan_id  = self.plan_id + 1
+    #     return str(self.plan_id)
 
-        resources = self.read_csv(self.resources_file)
-        camp_exists = False
-        for column in resources:
-            if column.get('camp_id') == self.camp_id:
-                camp_exists = True
-                if resource_type in column:
-                    column[resource_type] = quantity
+## This is to refresh the plan after creating a plan 
+    def insert_new_plan(self, new_plan):
+        self.plan_list.append(new_plan)
+
+    
+
+
+
+
+
+
+    
+    def set_camp_id(self):
+        self.camp_id = input("Enter camp ID (Not case sensitive)): ").upper()
+
+
+    def get_camp_population(self):
+            try:
+                pd_obj = pd.read_csv(self.camps_file)
+                # pd_obj is the name for dataframe and we treat it 
+                # like array thus result below
+                camp = pd_obj[pd_obj['Camp_ID'].str.upper() == self.camp_id]
+                if not camp.empty:
+                    no_ref = int(camp.iloc[0]['Num_Of_Refugees'])
+                    no_volunteer = int(camp.iloc[0]['Num_Of_Volunteers'])
+                    return no_ref
+                else:
+                    print(f"Camp ID: {self.camp_id} not found.")
+                    return 0
+            except Exception as e:
+                print(f"Error in get_camp_population: {e}")
+                return 0
+            
+
+    def update_resource_allocation(self, resource_type, quantity):
+        try:
+            if not isinstance(self.camp_id, str) or not isinstance(resource_type, str):
+                raise ValueError("camp_id and resource_type must be strings")
+
+            resources = pd.read_csv(self.resources_file)
+            camps = pd.read_csv(self.camps_file)
+            if self.camp_id in camps['Camp_ID'].values and self.camp_id in resources['Camp_ID'].values:
+                if resource_type in resources.columns:
+                    resources.loc[resources['Camp_ID'] == self.camp_id, resource_type] = quantity
                     print(f"Updated {resource_type} for {self.camp_id} to {quantity}.")
-                    self.write_csv(self.resources_file, resources)
-                    return
+                    resources.to_csv(self.resources_file, index=False)
                 else:
                     raise ValueError(f"Invalid resource type: {resource_type}")
+            else:
+                print(f"Camp ID {self.camp_id} does not exist in the resources or camps file.")
 
-        if not camp_exists:
-            print(f"Camp ID {self.camp_id} does not exist in the file.")
+        except Exception as e:
+            print(f"Error in update_resource_allocation: {e}")
 
-    except Exception as e:
-        print(f"Error in update_resource_allocation: {e}")
+    def suggest_resources(self, duration_days=7):
+        population = self.get_camp_population()
+        if population > 0:
+            food_per_person_per_day = 3
+            medical_kits_per_10_people_per_day = 0.1
+            people_per_tent = 5
 
+            total_food_needed = food_per_person_per_day * population * duration_days
+            total_medical_kits_needed = medical_kits_per_10_people_per_day * population * duration_days
+            total_tents_needed = -(-population // people_per_tent)
 
-  def suggest_resources(self, duration_days=7):
-    population = self.get_camp_population()
-    if population > 0:
-        # Define basic needs
-        food_per_person_per_day = 3  # e.g., 3 food packets per person per day
-        medical_kits_per_10_people_per_day = 0.1  # e.g., 1 kit per 10 people per day
-        people_per_tent = 5  # e.g., Assuming 5 people per tent
+            buffer = 1.20
+            total_food_needed = int(total_food_needed * buffer)
+            total_medical_kits_needed = int(total_medical_kits_needed * buffer)
+            total_tents_needed = int(total_tents_needed * buffer)
 
-        # Calculate total needs
-        total_food_needed = food_per_person_per_day * population * duration_days
-        total_medical_kits_needed = medical_kits_per_10_people_per_day * population * duration_days
-        total_tents_needed = -(-population // people_per_tent)  # Ceiling division for tents
+            return {"food_pac": total_food_needed, "medical_sup": total_medical_kits_needed, "tents": total_tents_needed}
+        else:
+            print(f"No suggestions for Camp_ID {self.camp_id} with population 0.")
+            return {}
 
-        # Add a buffer (e.g., 20%)
-        buffer = 1.20
-        total_food_needed = int(total_food_needed * buffer)
-        total_medical_kits_needed = int(total_medical_kits_needed * buffer)
-        total_tents_needed = int(total_tents_needed * buffer)
+    def manual_resource_allocation(self):
+        if not self.camp_id:
+            self.set_camp_id()
+        suggested_resources = self.suggest_resources()
+        for resource_type, suggested_quantity in suggested_resources.items():
+            print(f"Suggested quantity for {resource_type}: {suggested_quantity}")
+            while True:
+                user_input = input(f"Enter quantity for {resource_type} (press Enter to accept suggestion): ")
+                if not user_input:
+                    quantity = suggested_quantity
+                    break
+                else:
+                    try:
+                        quantity = int(user_input)
+                        if quantity >= 0:
+                            break
+                        else:
+                            print("Please enter a non-negative integer.")
+                    except ValueError:
+                        print("Invalid input. Please enter a valid integer.")
+            self.update_resource_allocation(resource_type, quantity)
 
-        return {"food_pac": total_food_needed, "medical_sup": total_medical_kits_needed, "tents": total_tents_needed}
-    else:
-        print(f"No suggestions for camp_id {self.camp_id} with population 0.")
-        return {}
-
-
-  def manual_resource_allocation(self):
-      if not self.camp_id:
-          self.set_camp_id()
-      suggested_resources = self.suggest_resources()
-      for resource_type, suggested_quantity in suggested_resources.items():
-          print(f"Suggested quantity for {resource_type}: {suggested_quantity}")
-          user_input = input(f"Enter quantity for {resource_type} (press Enter to accept suggestion): ")
-          quantity = int(user_input) if user_input else suggested_quantity
-          self.update_resource_allocation(resource_type, quantity)
-
-# This code is for set date of the plan,but still a draft(something need to revise) because without the whole plan table
-  def set_valid_sdate(self):
-        current_date = datetime.datetime.now()
-        cur_year = current_date.year
-        cur_month = current_date.month
-        cur_day = current_date.day
-        self.year = int(input('choose the year of the start date'))
-        while self.year < cur_year:
-            self.year = int(input('the year is invalid,please choose the year again'))
-        self.month = int(input('choose the month of the start date'))
-        while self.month < cur_month:
-            self.month = int(input('the month is invalid,please choose the year again'))
-        self.day = int(input('choose the day of the start date '))
-        while self.day < cur_day:
-            self.day = int(input('the day is invalid,please choose the year again'))
-
-        date_s = datetime.datetime(self.year, self.month, self.day)
-
-        return date_s
-
-  def set_valid_eedate(self, sdate_):
-        current_date = datetime.datetime.now()
-        sdate_year = sdate_.year
-        sdate_month = sdate_.month
-        sdate_day = sdate_.day
-        print(sdate_year)
-        self.year = int(input('choose the year of the start date'))
-        while self.year < sdate_year:
-            self.year = int(input(f'the start year of this plan is {sdate_year},please choose the year again'))
-        self.month = int(input('choose the month of the start date'))
-        while self.month < sdate_month:
-            self.month = int(input(f'the start month of this plan is {sdate_month},please choose the month again'))
-        self.day = int(input('choose the day of the start date '))
-        while self.day < sdate_day:
-            self.day = int(input(f'the start day of this plan is {sdate_day},please choose the day again'))
-
-        date_e = datetime.datetime(self.year, self.month, self.day)
-
-        return date_e
-
-  def read_date(self):
-        with open('plan_date.CSV', 'r') as date_file:
-            read = csv.DictReader(date_file)
-            for row in read:
-                print(row['start_date'], row['end_date'])
-
-def save_date(sdate_save, edate_save):
-    sdate_save = sdate_save.strftime('%Y-%m-%d')
-    edate_save = edate_save.strftime('%Y-%m-%d')
-    print(sdate_save)
-    print(edate_save)
-
-    with open('plan_date.CSV', 'w') as date_file:
-        headers = ['start_date', 'end_date']
-        save_date_ = csv.DictWriter(date_file, fieldnames=headers)
-        save_date_.writeheader()
-        date_dic = {'start_date': sdate_save, 'end_date': edate_save}
-        save_date_.writerow(date_dic)
 
 
 if __name__ == "__main__":
     admin = Admin()
-    admin.set_camp_id()  # Set camp_id once
-    admin.manual_resource_allocation() 
-
-    sdate = Admin().set_valid_sdate()
-    edate = Admin().set_valid_eedate(sdate)
-    save_date(sdate, edate)
-    Admin().read_date()
+    admin.set_camp_id()
+    population = admin.get_camp_population()
+    print(f"Number of refugees for camp {admin.camp_id}: {population}")
+    admin.manual_resource_allocation()
