99a9084f12c4209fedb285659c1e04b739139ff4
GracieGe <ruiyi.ge2001@gmail.com>
2023-11-24 00:08:27 +0000

    gui update

--- classes/Gui.py
+++ classes/Gui.py
@@ -3,76 +3,81 @@ from tkinter import messagebox
 import csv
 import re
 
-# draft (have some errors and haven't been defined as a class.)
+class UserRegistration:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("User Registration")
 
-def main():
-    register_gui()
+        self.username_label = tk.Label(self.root, text = "Username:\n\nYour username should be between 8 and 16 characters long and only consist of letters a-z and numbers 0-9")
+        self.username_entry = tk.Entry(self.root)
 
-def register_gui():
-    root = tk.Tk()
-    root.title("User Registration")
+        self.first_name_label = tk.Label(self.root, text = "\nFirst Name:")
+        self.first_name_entry = tk.Entry(self.root)
+    
+        self.password_label = tk.Label(self.root, text = "\nPassword:\n\nYour password should contain at least one capital letter, at least one of '?' or '!',\nletters a-z and numbers 0-9 and be between 8 and 16 characters long")
+        self.password_entry = tk.Entry(self.root, show = "*")
 
-    username_label = tk.Label(root, text = "Your username should be between 8 and 16 characters long and only consist of letters a-z and numbers 0-9.\n\nPlease enter a username:")
-    username_entry = tk.Entry(root)
+        self.register_button = tk.Button(self.root, text = "Register", command=self.register)
 
-    password_label = tk.Label(root, text = "\nYour password should contain at least one capital letter, at least one of '?' or '!', letters a-z and numbers 0-9 and be between 8 and 16 characters long.\n\nPlease enter a password: ")
-    password_entry = tk.Entry(root, show = "*")
+        self.username_label.pack()
+        self.username_entry.pack()
 
-    register_button = tk.Button(root, text = "Register", command = lambda: register(username_entry.get(), password_entry.get(), root))
+        self.first_name_label.pack()
+        self.first_name_entry.pack()
 
-    username_label.pack()
-    username_entry.pack()
+        self.password_label.pack()
+        self.password_entry.pack()
 
-    password_label.pack()
-    password_entry.pack()
+        self.register_button.pack()
 
-    register_button.pack()
+        self.root.mainloop()
 
-    root.mainloop()
+    def register (self):
+        username = self.username_entry.get()
+        password = self.password_entry.get()
 
-def register (username, password,root):
-    try:
-        validate_username(username)
-        validate_password(password)
+        try:
+            self.validate_username(username)
+            self.validate_password(password)
          
-        with open ("../files/logindetails.csv", "a") as file:
-            file.write(f"{username},{password},{True}\n")
+            with open ("intro-programming/files/logindetails.csv", "a") as file:
+                file.write(f"{username},{password},{True}\n")
         
-        messagebox.showinfo("Success", "Registration successful!")
-        root.destroy()
+            messagebox.showinfo("Success", "Registration successful!")
+            self.root.destroy()
 
-    except ValueError as e:
-        messagebox.showerror("Error", str(e))
+        except ValueError as e:
+            messagebox.showerror("Error", str(e))
 
-def validate_username(username):
-    with open("../files/logindetails.csv", "r") as file:
-        file_reader = csv.reader(file)
-        next(file_reader)
-        for row in file_reader:
-            if username == row[0]:
-                raise ValueError("This username already exists. Please try an alternative username.")
-            else:
-                continue
+    def validate_username(self,username):
+        with open("intro-programming/files/logindetails.csv", "r") as file:
+            file_reader = csv.reader(file)
+            next(file_reader)
+            for row in file_reader:
+                if username == row[0]:
+                    raise ValueError("This username already exists. Please try an alternative username.")
+                else:
+                    continue
             
-    if " " in username:
-        raise ValueError("Do not enter spaces in your username.")
-    elif len(username) < 8 or len(username) > 16:
-        raise ValueError("Please ensure that your username is between 8 and 16 characters long.")
-    elif username.isalnum() == False:
-        raise ValueError("Please ensure that your username contains only numbers and letters.")
+        if " " in username:
+            raise ValueError("Do not enter spaces in your username.")
+        elif len(username) < 8 or len(username) > 16:
+            raise ValueError("Please ensure that your username is between 8 and 16 characters long.")
+        elif username.isalnum() == False:
+            raise ValueError("Please ensure that your username contains only numbers and letters.")
 
-def validate_password(password):
-    pw_chars = r'^[A-Za-z0-9!?]+$'
-    punc_chars = r'[!?]'
-    if " " in password:
-        raise ValueError("Do not enter spaces in your password.")
-    elif len(password) < 8 or len(password) > 16:
-        raise ValueError("Please ensure that your password is between 8 and 16 characters long.")
-    elif not any(c.isupper() for c in password):
-        raise ValueError("Please ensure that your password contains at least one capital letter.")
-    elif not re.match(pw_chars, password) or not re.search(punc_chars, password):
-        raise ValueError("Please ensure that your password contains only letters a-z, numbers 0-9, and at least one of '!' or '?'.")
+    def validate_password(self,password):
+        pw_chars = r'^[A-Za-z0-9!?]+$'
+        punc_chars = r'[!?]'
+        if " " in password:
+            raise ValueError("Do not enter spaces in your password.")
+        elif len(password) < 8 or len(password) > 16:
+            raise ValueError("Please ensure that your password is between 8 and 16 characters long.")
+        elif not any(c.isupper() for c in password):
+            raise ValueError("Please ensure that your password contains at least one capital letter.")
+        elif not re.match(pw_chars, password) or not re.search(punc_chars, password):
+            raise ValueError("Please ensure that your password contains only letters a-z, numbers 0-9, and at least one of '!' or '?'.")
 
 if __name__ == "__main__":
-    main()
+    registration = UserRegistration()
 
